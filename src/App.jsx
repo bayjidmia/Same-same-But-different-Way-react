b9406e25998d6cb680e353a09485d6abf328a0e1
bayjidmia <mdbayjidmia982@gmail.com>
2025-09-28 11:21:14 +0600

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <ul className="flex">
+          <li className="mr-10">
+            <a href="">Blog</a>
+          </li>
+          <li className="mr-10">
+            <a href="">Post</a>
+          </li>
+          <li className="mr-10">
+            <a href="">Faq</a>
+          </li>
+          <li className="mr-10">
+            <a href="">Learn</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default App;
